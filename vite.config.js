e091ba4f51107054e6ed31d5c0440bb710ac67cf
zhenmunse <2711675247@qq.com>
2025-10-19 21:31:20 -0700

    fix: 修改为根路径部署，支持 main 和 vue-test 分支切换

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.VITE_BASE || '/run_global/vue-test/',
+  base: process.env.VITE_BASE || '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
